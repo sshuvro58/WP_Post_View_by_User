243e285edecb4f178f4c4c17d53206975330d6c4
sshuvro58 <sshuvro58@gmail.com>
2015-08-19 10:33:00 +0600

    Update post-view-by-user.php

--- post-view-by-user.php
+++ post-view-by-user.php
@@ -5,6 +5,19 @@ Description: This plugin will help you to check if a registered user has viewed 
 Author: Mithun Sarker Shuvro
 Version: 1.0
 License: GPLv2
+Post View by User is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+Post View by User is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Post View by User.
+
 */
 
 // function to display number of posts.
