fe5c024ada6cbe592f63f12bcd7c64faa0d62f4c
sshuvro58 <sshuvro58@gmail.com>
2015-08-19 10:02:46 +0600

    Update post-view-by-user.php

--- post-view-by-user.php
+++ post-view-by-user.php
@@ -8,7 +8,7 @@ License: GPLv2
 */
 
 // function to display number of posts.
-function getPostViews($postID){
+function pvbu_getPostViews($postID){
     $count_key = 'post_views_by_user';
     $count = get_post_meta($postID, $count_key, true);   
     $total_item = count($count);
@@ -35,7 +35,7 @@ function getPostViews($postID){
 }
  
 // function to count views.
-function setPostViews($postID) {
+function pvbu_setPostViews($postID) {
     $count_key = 'post_views_by_user';
     $count = get_post_meta($postID, $count_key, true);
     //echo '<script type="text/javascript">console.log('.$count.')</script>';
@@ -67,13 +67,13 @@ function setPostViews($postID) {
  
  
 // Add it to a column in WP-Admin
-add_filter('manage_posts_columns', 'posts_column_views');
-add_action('manage_posts_custom_column', 'posts_custom_column_views',5,2);
-function posts_column_views($defaults){
+add_filter('manage_posts_columns', 'pvbu_posts_column_views');
+add_action('manage_posts_custom_column', 'pvbu_posts_custom_column_views',5,2);
+function pvbu_posts_column_views($defaults){
     $defaults['post_views'] = __('Viewed by');
     return $defaults;
 }
-function posts_custom_column_views($column_name){
+function pvbu_posts_custom_column_views($column_name){
     if($column_name === 'post_views'){
         echo getPostViews(get_the_ID());
     }
@@ -81,10 +81,10 @@ function posts_custom_column_views($column_name){
 
 
 
-function work(){
+function pvbu_work(){
 	if(is_single(get_the_ID())){
 			setPostViews(get_the_ID());
 	}
 }
 
-add_action('wp_head','work');
+add_action('wp_head','pvbu_work');
